8a3e5a1ed3c4651f45b8af9eb882dfe00b6537fc
Kail Tian <tianjinbaoemail@163.com>
2025-11-24 18:02:14 +0800

    更新agentState

---
+++ core/agentState.py
@@ -0,0 +1,16 @@
+from typing import TypedDict, Dict, Annotated, List, NotRequired
+
+class AgentState(TypedDict):
+    sql_id:str  #id,初始化数据
+    query: str  # 原始问题,初始化数据
+    schema: str  # 检索到的Schema,rerank后更新
+    knowledge: str  #相关知识,初始化数据
+    thinking: NotRequired[str]  # 当前思考
+    action: str  # 最近执行动作(generate_sql/execute_sql/fix_sql),node中更新
+    sql_state: str #success 或者 error,执行时更新
+    current_sql: str  # 生成的SQL,refiner时更新
+    error_msg: str #执行错误信息,执行时更新
+    execution_result: NotRequired[Dict] # 执行结果
+    error_count: int # 错误次数，当错误次数为3时停止,执行时更新
+    final_sql: str  # 最终SQL,执行时更新
+    conversation_history:NotRequired[List[Dict[str, str]]]
