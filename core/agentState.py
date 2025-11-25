1a86d62f5da9f87821db6becfb79c4473a547185
Kail Tian <tianjinbaoemail@163.com>
2025-11-25 14:45:18 +0800

    update

--- core/agentState.py
+++ core/agentState.py
@@ -1,10 +1,10 @@
 from typing import TypedDict, Dict, Annotated, List, NotRequired
 
 class AgentState(TypedDict):
-    sql_id:str  #id,初始化数据
+    sql_id: str  # id,初始化数据
     query: str  # 原始问题,初始化数据
-    schema: str  # 检索到的Schema,rerank后更新
-    knowledge: str  #相关知识,初始化数据
+    schema: list  # 检索到的Schema,rerank时更新
+    knowledge_rules: str  #相关知识,初始化数据,rerank时更新
     thinking: NotRequired[str]  # 当前思考
     action: str  # 最近执行动作(generate_sql/execute_sql/fix_sql),node中更新
     sql_state: str #success 或者 error,执行时更新
@@ -12,5 +12,5 @@ class AgentState(TypedDict):
     error_msg: str #执行错误信息,执行时更新
     execution_result: NotRequired[Dict] # 执行结果
     error_count: int # 错误次数，当错误次数为3时停止,执行时更新
-    final_sql: str  # 最终SQL,执行时更新
+    final_sql: NotRequired[str]  # 最终SQL,执行时更新，refined后更新，暂时不用这个字段
     conversation_history:NotRequired[List[Dict[str, str]]]
